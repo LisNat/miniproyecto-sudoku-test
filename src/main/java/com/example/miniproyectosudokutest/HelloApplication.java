7e65addf2088a0a6a1afdd145d6d41b28f918691
Liseth Natalia Rivera Córdoba <rivera.liseth@correounivalle.edu.co>
2025-04-03 16:45:40 -0500

    Código arreglado

--- src/main/java/com/example/miniproyectosudokutest/HelloApplication.java
+++ src/main/java/com/example/miniproyectosudokutest/HelloApplication.java
@@ -11,6 +11,9 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 300,200);
+        stage.setTitle("Application!");
+        stage.setScene(scene);
         stage.show();
     }
 
